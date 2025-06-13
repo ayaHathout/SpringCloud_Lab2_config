c83717b7c25b04c74b19cddfe4d8fd43ee06b0ab
Aya <ayahathout1@gmail.com>
2025-06-14 00:16:46 +0300

    remove some

--- microservice1/src/main/java/com/example/microservice1/dtos/UserDTO.java
+++
@@ -1,7 +0,0 @@
-package com.example.microservice1.dtos;
-
-import java.io.Serializable;
-
-public record UserDTO(String fullName, String email) implements Serializable {
-    // The canonical constructor is created by default
-}
--- microservice1/src/main/java/com/example/microservice1/repositories/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.microservice1.repositories;
-
-import com.example.microservice1.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {}
